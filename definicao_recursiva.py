06a90e65962b6eccfbfcfcf70f799eb78ae0e9ea
Daniel <maffezzolidaniel78@gmail.com>
2023-09-23 17:43:27 -0300

    .

--- definicao_recursiva.py
+++ definicao_recursiva.py
@@ -3,34 +3,28 @@ def mostra(f_valor,calcula,n): #função que facilita a reutilização de linhas
     print(f_valor[n])
 
 def primeiro_slide():
-    f = [0]
     f_valor = [3]
     n = 1
     while n <= 15:
-        f.append(n)
-        calcula = 2*f_valor[f[n] - 1] + 3
+        calcula = 2*f_valor[n- 1] + 3
         mostra(f_valor,calcula,n)
         n += 1
-#primeiro_slide()
+primeiro_slide()
 
 def segundo_slide():
-    f = [0]
     f_valor = [1]
     n = 1
     while n <= 10:
-        f.append(n)
-        calcula = 10 * f_valor[f[n] - 1]
+        calcula = 10 * f_valor[n - 1]
         mostra(f_valor,calcula,n)
         n +=1
-#segundo_slide()
+segundo_slide()
 
 def quarto_slide():
-    f = [0,1]
     f_valor = [1,1]
     n = 2
     while n <= 20:
-        f.append(n)
-        calcula = f_valor[f[n] - 1] + f_valor[f[n] - 2]
+        calcula = f_valor[n - 1] + f_valor[n - 2]
         mostra(f_valor,calcula,n)
         n += 1
 
